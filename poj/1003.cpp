2889a4b10fb1c5f798616d3bbbee9da8c04346bb
banbao990 <43907877+banbao990@users.noreply.github.com>
2020-01-14 11:36:06 +0800

    Create 1003.cpp

---
+++ poj/1003.cpp
@@ -0,0 +1,36 @@
+/**********************************************************************
+ * author:      banbao                                                *
+ * language:    c++/c                                                 *
+ * link:        http://poj.org/problem?id=1003                        *
+ **********************************************************************/
+
+/*
+ * bbq: 输入a, 输出使得 1/2 + 1/3 + 1/4 + ... + 1/(n + 1) >= a 的最小n
+ *
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double test;
+    double sum = 0;
+    double i = 2;
+    while (1) {
+        scanf("%lf", &test);
+        if (fabs(test) > 1e-6) {
+            sum = 0;
+            i = 2;
+            while (1) {
+                sum += 1 / i;
+                if (sum >= test) {
+                    printf("%d card(s)\n", (int)(i - 1));
+                    break;
+                }
+                ++i;
+            }
+        }
+        else break;
+    }
+    return 0;
+}
